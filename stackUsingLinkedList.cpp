1f14a5f68bee2d849286afe6fe710e52e8fa3142
yara22aulia <yara22aulia@gmail.com>
2026-05-14 23:58:59 +0700

    Menyelesaikan program stack linked list sederhana

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -89,8 +89,7 @@ int main()
             }
             break;
         case 3:
-            if (!stack.isEmpty())
-            {
+            if (!stack.isEmpty()){
                 stack.peek();
             }
             else
