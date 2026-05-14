9147b065d9d00ce1019f50dea48fd5f4e3ba7e98
yara22aulia <yara22aulia@gmail.com>
2026-05-14 23:49:40 +0700

    Mengimplementasikan penghapusan elemen teratas stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -42,8 +42,7 @@ public:
         delete temp;}
 
     void peek(){
-        if (top == NULL)
-        {
+        if (top == NULL){
             cout << "List is empty." << endl;
         }
         else
