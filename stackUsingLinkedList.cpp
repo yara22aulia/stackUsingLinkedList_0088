fe28b5910fecda5438eef4d5506db7d3d4ca6986
yara22aulia <yara22aulia@gmail.com>
2026-05-14 23:57:22 +0700

    Menambahkan validasi pilihan menu stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -81,8 +81,7 @@ int main()
             stack.push(value);
             break;
         case 2:
-            if (!stack.isEmpty())
-            {
+            if (!stack.isEmpty()){
                 stack.pop();
             }
             else
