32feb17172b4337e668333936f87b98946cfc867
yara22aulia <yara22aulia@gmail.com>
2026-05-14 23:56:20 +0700

    Menambahkan fitur input pilihan menu

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -74,8 +74,7 @@ int main()
         cout << "Enter your choice: ";
         cin >> choice;
 
-        switch (choice)
-        {
+        switch (choice){
         case 1:
             cout << "Enter the value to push: ";
             cin >> value;
